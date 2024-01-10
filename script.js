79ce93f2b8c915d8f52a09e90be9d600eed61421
sudhamshu2895 <118543726+sudhamshu2895@users.noreply.github.com>
2024-01-10 12:40:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,17 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
-        // sticky navbar on scroll script
-        if(this.scrollY > 20){
-            $('.navbar').addClass("sticky");
-        }else{
-            $('.navbar').removeClass("sticky");
-        }
-        
-        // scroll-up button show/hide script
-        if(this.scrollY > 500){
-            $('.scroll-up-btn').addClass("show");
-        }else{
-            $('.scroll-up-btn').removeClass("show");
-        }
-    });
+const hamburger = document.getElementById('hamburger'); 
+const menu = document.querySelector('.menu'); 
 
-    // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
-        // removing smooth scroll on slide-up button click
-        $('html').css("scrollBehavior", "auto");
-    });
-
-    $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
-        $('html').css("scrollBehavior", "smooth");
-    });
-
-    // toggle menu/navbar script
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
-
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-
-    // owl carousel script
-    $('.carousel').owlCarousel({
-        margin: 20,
-        loop: true,
-        autoplay: true,
-        autoplayTimeOut: 2000,
-        autoplayHoverPause: true,
-        responsive: {
-            0:{
-                items: 1,
-                nav: false
-            },
-            600:{
-                items: 2,
-                nav: false
-            },
-            1000:{
-                items: 3,
-                nav: false
-            }
-        }
-    });
+hamburger.addEventListener('click', function () { 
+	const hamIcon = this.querySelector('.hamburger-icon'); 
+	const crossIcon = this.querySelector('.cross-icon'); 
+	if (hamIcon.style.display === "none") { 
+		hamIcon.style.display = "inline-block"
+		menu.style.display = "none"
+		crossIcon.style.display = "none"
+	} 
+	else { 
+		crossIcon.style.display = "inline-block"
+		hamIcon.style.display = "none"
+		menu.style.display = "block"
+	} 
 });
