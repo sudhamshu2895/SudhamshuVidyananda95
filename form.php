42f4b8b3914d422bd59919aa1779c6ad5120dfc8
sudhamshu2895 <118543726+sudhamshu2895@users.noreply.github.com>
2024-01-10 19:18:24 -0500

    Update form.php

--- form.php
+++ form.php
@@ -1,34 +1,17 @@
 <?php
-$action=$_REQUEST['action'];
-if ($action=="") /* display the contact form */
-    {
-    ?>
-    <form action="" method="POST" enctype="multipart/form-data">
-    <input type="hidden" name="action" value="submit">
-    Your name:<br>
-    <input name="name" type="text" value="" size="30"/><br>
-    Your email:<br>
-    <input name="email" type="text" value="" size="30"/><br>
-    Your message:<br>
-    <textarea name="message" rows="7" cols="30"></textarea><br>
-    <input type="submit" value="Send email"/>
-    </form>
-    <?php
-    } 
-else /* send the submitted data */
-    {
-    $name=$_REQUEST['name'];
-    $email=$_REQUEST['email'];
-    $message=$_REQUEST['message'];
-    if (($name=="")||($email=="")||($message==""))
-        {
-        echo "All fields are required, please fill <a href=\"\">the form</a> again.";
-        }
-    else{        
-        $from="From: $name<$email>\r\nReturn-path: $email";
-        $subject=" Message sent using your contact form";
-        mail("vidyananda.s@northeastern.edu", $subject, $message, $from);
-        echo "Email sent!";
-        }
-    }  
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $to = 'your@email.com';
+    $headers = 'From: ' . $email;
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "<p>Email sent!</p>";
+    } else {
+        echo "<p>Email failed to send.</p>";
+    }
+}
 ?>
